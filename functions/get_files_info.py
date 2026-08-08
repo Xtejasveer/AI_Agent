23696ad0b87ec9ce6a532148b56f910d6edab743
Tejasveer Singh Sodhi <tejasveer2020@gmail.com>
2026-08-08 20:38:18 +0530

    Giving LLM access to our tools..

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -20,3 +20,21 @@ def get_files_info (working_directory, directory = None):
         size = os.path.getsize(content_path)
         final_response += f" - {content}: file_size = {size} bytes, is_dir = {is_dir}\n"
     return final_response
+
+
+schema_get_files_info = {
+    "type": "function",
+    "function": {
+        "name": "get_files_info",
+        "description": "Lists files in a specified directory relative to the working directory, providing file size and directory status",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "directory": {
+                    "type": "string",
+                    "description": "Directory path to list files from, relative to the working directory (default is the working directory itself)",
+                },
+            },
+        },
+    },
+}
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os 
+import json
 from dotenv import load_dotenv
 from openai import OpenAI
 import sys
-from functions.get_files_info import get_files_info
+from functions.get_files_info import schema_get_files_info
 
 def main():
 
@@ -13,6 +14,15 @@ def main():
         api_key = os.environ["OPENROUTER_API_KEY"],
         base_url = "https://openrouter.ai/api/v1"
     )
+
+    system_prompt = (
+        """You are a helpful AI coding agent.
+        When a user asks a question or makes a request, amke afunction call plan. You can perform the following operations:
+        - List files adn directories
+         
+        All paths you provide should be relative to the working directory. You donot specify the wroking directory in your function calls as it is automatically injected for security reasons."""
+    )
+
     if len(sys.argv) < 2 :
         print("I need a prompt")
         sys.exit(1)
@@ -21,17 +31,21 @@ def main():
         verbose_flag = True
         
     prompt = sys.argv[1]
-    messages = [{"role" : "user", "content" :prompt}]
+    messages = [{"role" : "user", "content" :prompt},
+                {"role" : "system", "content" : system_prompt}]
 
-
+    available_functions = [
+        schema_get_files_info,
+    ]
     
     print ("Args :", sys.argv)
     response = client.chat.completions.create(
         messages = messages,
-        model = "google/gemma-4-26b-a4b-it:free"
+        model = "google/gemma-4-26b-a4b-it:free",
+        temperature= 0,
+        tools = available_functions   
     )
 
-    print(response.choices[0].message.content)
     if response is None or response.usage is None:
         print("response is malformed")
         return 
@@ -39,5 +53,14 @@ def main():
     if verbose_flag : 
         print(f"User Prompt : {prompt}")
         print(f"Prompt Tokens : {response.usage.total_tokens}")
+
+    message = response.choices[0].message
+
+    if not message.tool_calls:
+        print(f"No tool calls made. The message content is : {message.content}")
+    else :
+        for tool_call in message.tool_calls:
+            function_args = json.loads(tool_call.function.arguments or "{}")
+            print(f"Calling Function: {tool_call.function.name}({function_args})")
 
 main()
